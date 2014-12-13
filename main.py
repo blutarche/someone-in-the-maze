2ca0dc28ed0138fa5462902c614eae163d3502c4
Aikdanai Sidhikosol <visitor19blut@gmail.com>
2014-12-13 23:28:05 +0700

    Time is 3 minutes

--- main.py
+++ main.py
@@ -18,34 +18,56 @@ class MazeGame(gamelib.SimpleGame):
     COLUMN = 73
 
     def __init__(self):
-        super(MazeGame, self).__init__('Someone in the Maze', MazeGame.BLACK, window_size=(MazeGame.WIDTH, MazeGame.HEIGHT))
+        super(MazeGame, self).__init__('3 minutes MAZE', MazeGame.BLACK, window_size=(MazeGame.WIDTH, MazeGame.HEIGHT))
         self.piece_size = MazeGame.WIDTH / MazeGame.COLUMN
         self.map = Map(row=MazeGame.ROW, 
                         column=MazeGame.COLUMN, 
                         piece_size=self.piece_size)
         self.player = Player(size=self.piece_size, pos=(1,1), color=MazeGame.RED, gamemap=self.map)
         self.is_started = False
+        self.is_end = False
+        self.last_ticks = 0
 
     def start_game(self):
         self.init()
         self.is_started = True
+        self.is_end = False
 
     def init(self):
         super(MazeGame, self).init()
-        self.time = 0.0
+        self.time = 180999
         self.render_string()
 
     def update(self):
-        self.time = pygame.time.get_ticks()/1000
-        print "TIME: "+str(self.time)
+        self.update_time()
+        self.render_time()
+
+    def update_time(self):
+        if self.is_started:
+            this_tick = pygame.time.get_ticks()
+            self.time -= this_tick - self.last_ticks
+        else:
+            pass
+        self.last_ticks = this_tick
 
     def render_string(self):
         self.render_time()
 
     def render_time(self):
-        self.time_image = self.font.render("Time = %d" % self.time, 0,MazeGame.WHITE)
+        time_in_sec = self.time/1000
+        seconds = time_in_sec % 60
+        minutes = time_in_sec / 60
+        self.time_image = self.font.render("Time %d:%02d" % (minutes, seconds), 0, MazeGame.WHITE)
 
     def render(self, surface):
+        if self.is_started:
+            self.render_game()
+        elif self.is_end:
+            
+        else:
+            pass
+
+    def render_game(self, surface):
         self.map.render(surface)
         self.player.render(surface)
         surface.blit(self.time_image, (20, MazeGame.HEIGHT - 50))
@@ -61,7 +83,8 @@ class MazeGame(gamelib.SimpleGame):
             if key == K_RIGHT:
                 self.player.right()
         else:
-            if key == K_RETURN:
+            if key == K_SPACE:
+                print "eiei"
                 self.start_game()
 
 def main():
